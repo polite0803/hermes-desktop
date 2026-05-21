4c7ee1da62ffa4c420ea4fe605f9aa4e27cb0585
刘小平 <polite0803@outlook.com>
2026-05-21 12:10:05 +0800

    Fix: remove tauri-plugin-updater causing crash on startup

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -211,7 +211,6 @@ pub fn run() {
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_notification::init())
-        .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_process::init())
         .setup(move |app| {
             let loc = locale.clone();
