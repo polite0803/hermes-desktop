6f3751ac3bf33733f58de15266d81bef919d91a3
刘小平 <polite0803@outlook.com>
2026-05-21 22:27:50 +0800

    Fix collapsed sidebar: show 'H' icon instead of deformed banner

--- src/renderer/src/screens/Layout/Layout.tsx
+++ src/renderer/src/screens/Layout/Layout.tsx
@@ -116,7 +116,10 @@ function Layout({ verifyWarning, onReinstall, onDismissVerifyWarning }: LayoutPr
     <div className="layout">
       <aside className={`sidebar ${collapsed ? "sidebar-collapsed" : ""}`}>
         <div className="sidebar-brand">
-          <img src={hermeslogo} height={collapsed ? 24 : 30} width={collapsed ? 24 : undefined} alt="" style={collapsed ? { objectFit: "contain" } : undefined} />
+          {collapsed
+            ? <div className="sidebar-brand-icon">H</div>
+            : <img src={hermeslogo} height={30} alt="" />
+          }
           <button className="sidebar-toggle" onClick={() => setCollapsed((c) => !c)} title={collapsed ? t("navigation.expand") : t("navigation.collapse")}>
             {collapsed ? <PanelLeftOpen size={14} /> : <PanelLeftClose size={14} />}
           </button>
